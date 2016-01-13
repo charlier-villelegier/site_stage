87a4086ad155c251b4df83bc5fd3e4f2a8b29cc6
charlier-villelegier <xaviervillelegier@MacBook-Pro-de-Xavier.local>
2016-01-13 14:59:46 +0100

    Controleurs

---
+++ modeles/bd.php
@@ -0,0 +1,24 @@
+﻿<?php
+	class Bd {
+		
+		private $bdd;
+		private $host = "localhost";
+		private $user = "root";
+		private $passwd = "";
+		private $co;
+		
+		function __construct($bdd){
+			$this->bdd = $bdd;
+		}
+	
+		public function connexion() {
+			$this->co = mysqli_connect($this->host , $this->user , $this->passwd, $this->bdd) or die("erreur de connexion");
+			return($this->co);
+		} 
+		
+		public function deconnexion() {
+			mysqli_close();
+		}
+	}
+	
+?>
---
+++ modeles/membre.php
@@ -0,0 +1,52 @@
+﻿<?php
+include 'bd.php';
+	class Membre {
+		
+		private $login;
+		private $mdp;
+		private $email;
+		private $fonction;
+		
+		function __construct(){
+			
+			$bd = new Bd("site_stage");
+			$co = $bd->connexion();
+         	$this->login=func_get_arg(0);
+			$this->mdpasse=func_get_arg(1);
+			$this->fonction=func_get_arg(2);
+					
+			switch($this->fonction){
+				case "etudiant":
+					$resultat = mysqli_query($co, "SELECT mail FROM etudiant WHERE login='$this->login' AND mdp='$this->mdp'");
+					break;
+							
+				case "enseignant":
+					$resultat = mysqli_query($co, "SELECT mail FROM enseignant WHERE login='$this->login' AND mdp='$this->mdp'");
+					break;
+							
+				case "tuteur":
+					$resultat = mysqli_query($co, "SELECT mail FROM tuteur_entreprise WHERE login='$this->login' AND mdp='$this->mdp'");
+					break;
+							
+			}
+					
+			$row = mysql_fetch_row($result);
+			$this->email=$row['mail'];
+		}
+		
+	
+		public function connexion() {
+			session_start();
+		} 
+		
+		public function modif_mdpasse($mdp) {
+			$this->mdp=$mdp;
+			mysqli_query($co, "UPDATE membres SET mdpasse=$this->mdpasse WHERE id=$this->id");	
+		}
+		
+		public function deconnexion() {
+			session_destroy();
+		} 
+	}
+	
+?>
